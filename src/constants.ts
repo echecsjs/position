89dbe8ce09c1217ff23bb318dad481c9d35d1a51
Adrian de la Rosa <github@brick.email>
2026-03-19 10:13:44 +0100

    style: apply prettier formatting

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,11 @@
 import { Position } from './position.js';
 
-
 import type { Piece, Square } from './types.js';
 
 const EMPTY_BOARD = new Map<Square, Piece>();
 
 const STARTING_POSITION = new Position();
 
-export {
-  
-  EMPTY_BOARD,
-  
-  
-  
-  
-  STARTING_POSITION,
-};
+export { EMPTY_BOARD, STARTING_POSITION };
 
-export {COLORS, FILES, PIECE_TYPES, RANKS, SQUARES} from './primitives.js';
+export { COLORS, FILES, PIECE_TYPES, RANKS, SQUARES } from './primitives.js';
